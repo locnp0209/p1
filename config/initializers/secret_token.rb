630e9ad05d0ded843128ceb22eeb6d51da781b8c
nguyen phuoc loc <loc.nguyen@techproulsionlabs.com>
2013-03-07 14:09:28 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P1::Application.config.secret_token = '24b385476286433656922ef85bf124e37b9b373473ff820324601cf9b1af147b3a6b1ed6d0911a3c6362b3a789457933f0b2e04d775002f004fb18d50c541a75'
